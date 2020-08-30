a60cfae34ae764ce3aefc2cbcb14f813ee78267f
Alikhan219 <merabhai786786@gmail.com>
2020-08-30 19:12:30 +0500

    adding the bootstrap and jquery scripts and cdns and firebase scripts

---
+++ login.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATm6KUstNr1kuTnBB6f0T-QVxCT4yXvZI",
+    authDomain: "general-34630.firebaseapp.com",
+    databaseURL: "https://general-34630.firebaseio.com",
+    projectId: "general-34630",
+    storageBucket: "general-34630.appspot.com",
+    messagingSenderId: "115369774170",
+    appId: "1:115369774170:web:e7b6c795f8b8ee0d0c5c63",
+    measurementId: "G-8YBTXMRDJF",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.auth();
